30f1d9e64796aec3eb32e11cb8d08d25d2405cba
codemonkeyak <jumpingfrog87@gmail.com>
2017-02-12 23:12:30 -0800

    Det of matrix-r1

--- MatrixTest.java
+++ MatrixTest.java
@@ -7,40 +7,58 @@ public class MatrixTest {
 		m.print();
 		m.fillRand();
 		System.out.println("-------matrix m ----------");
-		m.print();
+	//	m.print();
 		System.out.println("");
     
 		Matrix n = new Matrix(2,3);
 		n.fillRand();
 		System.out.println("-------matrix n----------");
-		n.print();
+	//	n.print();
 		System.out.println("");
  	
 		System.out.println("--------add m and n-------");
 		m.add(n);
-		m.print();
+	//	m.print();
 		System.out.println("");
 		
 		System.out.println("--------product-------");
 		m.multiply(3);
-		m.print();
+	//	m.print();
 	 System.out.println("");
 
 	  System.out.println("--------transpose-----------");
 		Matrix p = new Matrix(2,3);
 		p.fillRand();
-		p.print();
+	//	p.print();
 		System.out.println("-------------------");
 		p.transpose();
-		p.print();
+	//	p.print();
 		System.out.println("");
 
 		System.out.println("--------identity-----------");
 		Matrix q = new Matrix(2,3);
-		q.identityMat(4);
-		q.print();
+		q.identityMat(3);
+	//	q.print();
 		System.out.println("");
 
+		System.out.println("--------matrix prod-----------");
+    Matrix x = new Matrix(2,3);
+		x.fillRand();
+		System.out.println("--------matrix x-----------");
+	//	x.print();
+		Matrix y = new Matrix(3,1);
+		y.fillRand();
+		System.out.println("--------matrix y-----------");
+	//	y.print();
+		x.matProduct(y);
+		System.out.println("--------matrix prod-----------");
+	//	x.print();
+	  System.out.println("--------submatrix-----------");
+		Matrix z = new Matrix(5,5);
+		z.fillRand();
+		z.print();
+		Matrix v = z.getSubMatrix(z,2);
+		v.print();
 	}
 
 
